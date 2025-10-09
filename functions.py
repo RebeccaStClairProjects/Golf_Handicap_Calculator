06453bed113be042e656426c1047149989aeb148
Rebecca St.Clair <rebecca.st.clair@snhu.edu>
2025-10-09 16:12:34 -0400

    Added a menu to select witch fetcher to test and moved the fetcer to add a new golfer into a new file.

--- functions.py
+++ functions.py
@@ -1,19 +1,14 @@
-## This is The start of somthing new 
 import mysql.connector
 conn = mysql.connector.connect(
     host= "localhost",
     username = "root",
     password = "BakuraCofh123",
     database = "golf"
-)
-
     ## ssl_ca = "path-to-ssl-certificate"  # often optional for simple setups
-
+)
 cursor = conn.cursor()
-print("Hello and welcome to polo Park East Men's Club Handicap Calculator")
-continu = "yes"
 
-while continu == "Yes" or continu == "yes":
+def add_new_golfer():
     G_rank = input("\nPleas enter first rank.  ")
     l_Name = input("\nPleas enter last name.  ")
     f_Name = input("\nPleas enter first name.  ")
@@ -21,13 +16,13 @@ while continu == "Yes" or continu == "yes":
     roundsPlayed = input("\nPleas enter The number of rounds you have played. ")
     roundAvg = input("\nPleas enter Your Round Avereg. ")
     seasonTotal = input("\nPleas enter Your season total. ")
-    farwell = "\nThank You " + f_Name + " " + l_Name + " for entering in your handicap of " + handicap + ".\n"
-    print(farwell)
+    ## cursor.execute("SELECT * FROM Golfers WHERE F_Name = %s AND L_Name = %s", (f_Name, l_Name))
     
+
     cursor.execute("INSERT INTO Golfer(G_rank, F_Name, L_Name, Handycap, R_Played, R_Avg, SeasonTotal) VALUES (%s, %s, %s, %s, %s, %s, %s)",
                (G_rank, f_Name, l_Name, handicap, roundsPlayed, roundAvg, seasonTotal))
+
     conn.commit()
-
+    farwell = "\nThank You " + f_Name + " " + l_Name + " for entering in your handicap of " + handicap + ".\n"
+    print(farwell)
     continu = input("do you wish to enter more handicaps?")
-
-print("Thank you for stoping by!")
---
+++ main.py
@@ -0,0 +1,30 @@
+## This is The start of somthing new 
+import functions
+continu = "yes"  
+option = 0
+
+while continu == "Yes" or continu == "yes":
+    
+    print("Hello and welcome to polo Park East Men's Club Handicap Calculator\n")
+    print("pleas select from the falowing options\n")
+    print("1: Add new Golfer\n")
+    print("2: Updat Golfer Information\n")
+    print("3: Add Round Score\n")
+    print("4: Display Curent Ranking\n")
+    
+    option = input("pleas select an option. ")
+
+    if option == "1":
+        functions.add_new_golfer()
+    elif option == "1":
+        print("\nThis option has not ben implumented")
+    elif option == "2":
+        print("\nThis option has not ben implumented")
+    elif option == "3":
+        print("\nThis option has not ben implumented")
+    elif option == "4":
+        print("\nThis option has not ben implumented")
+
+    continu = input("\nDo you want to chose another option?")
+
+print("\nThank you for stoping by!")
