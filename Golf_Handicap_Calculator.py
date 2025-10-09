43391ca7092aefc45cd01aa8e0694e4581142433
Rebecca St.Clair <rebecca.st.clair@snhu.edu>
2025-10-09 14:06:11 -0400

    Got Python integrated with a MySQL server on my lockal sistem. Will need to update latter to a cloud service.

--- Golf_Handicap_Calculator.py
+++ Golf_Handicap_Calculator.py
@@ -1,3 +1,33 @@
-
 ## This is The start of somthing new 
-print("Hello, world!")
+import mysql.connector
+conn = mysql.connector.connect(
+    host= "localhost",
+    username = "root",
+    password = "BakuraCofh123",
+    database = "golf"
+)
+
+    ## ssl_ca = "path-to-ssl-certificate"  # often optional for simple setups
+
+cursor = conn.cursor()
+print("Hello and welcome to polo Park East Men's Club Handicap Calculator")
+continu = "yes"
+
+while continu == "Yes" or continu == "yes":
+    G_rank = input("\nPleas enter first rank.  ")
+    l_Name = input("\nPleas enter last name.  ")
+    f_Name = input("\nPleas enter first name.  ")
+    handicap = input("\nPleas enter handicap. ")
+    roundsPlayed = input("\nPleas enter The number of rounds you have played. ")
+    roundAvg = input("\nPleas enter Your Round Avereg. ")
+    seasonTotal = input("\nPleas enter Your season total. ")
+    farwell = "\nThank You " + f_Name + " " + l_Name + " for entering in your handicap of " + handicap + ".\n"
+    print(farwell)
+    
+    cursor.execute("INSERT INTO Golfer(G_rank, F_Name, L_Name, Handycap, R_Played, R_Avg, SeasonTotal) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+               (G_rank, f_Name, l_Name, handicap, roundsPlayed, roundAvg, seasonTotal))
+    conn.commit()
+
+    continu = input("do you wish to enter more handicaps?")
+
+print("Thank you for stoping by!")
---
+++ SqlQuery_Set_Up.sql
@@ -0,0 +1,20 @@
+﻿CREATE Database;
+
+Show Databases;
+
+
+/*
+Use golf;
+Show tables;
+-- */
+/*
+	Create table Golfer( G_ID int unsigned auto_increment primary key not null, 
+					  F_Name varchar (30) not null,
+                      L_Name varchar (30) not null,
+                      email varchar (30) not null,
+					  Age int, 
+                      Handycap float,
+                      
+                      provitional BOOLEAN,
+                      Stableford int); 
+-- */
