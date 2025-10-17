7b34c5a9d706fc28dd3d190d40fa927b331e51d0
Victor St. Clair <vicstclair1@hotmail.com>
2025-10-17 14:03:45 -0400

    wording changes in prompts and comments

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ option = 0
 
 while continu == "Yes" or continu == "yes":
     
-    print("Hello and welcome to polo Park East Men's Club Handicap Calculator\n")
-    print("pleas select from the falowing options\n")
+    print("Hello and welcome to Polo Park East Men's Club Handicap Calculator\n")
+    print("please select from the following options\n")
     print("1: Add new Golfer\n")
-    print("2: Updat Golfer Information\n")
+    print("2: Update Golfer Information\n")
     print("3: Add Round Score\n")
-    print("4: Display Curent Ranking\n")
+    print("4: Display Current Ranking\n")
     
-    option = input("pleas select an option. ")
+    option = input("please select an option. ")
 
     if option == "1":
         functions.add_new_golfer()
     elif option == "2":
-        print("\nThis option has not ben implumented")
+        print("\nThis option has not been implemented")
     elif option == "3":
         functions.Add_New_Score()
     elif option == "4":
-        print("\nThis option has not ben implumented")
+        print("\nThis option has not been implemented")
     elif option == "5":
-        print("\nThis option has not ben implumented")
+        print("\nThis option has not been implemented")
 
     continu = input("\nDo you want to chose another option?")
 
-print("\nThank you for stoping by!")
+print("\nThank you for stopping by!")
